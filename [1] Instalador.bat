584a53e74db553bfb1e6bb45cf4669c6f8910520
PolarLofy <75093985+PolarLofy@users.noreply.github.com>
2022-05-15 21:46:07 -0300

    Add files via upload

---
+++ [1] Instalador.bat
@@ -0,0 +1,42 @@
+@echo off
+color A
+title Lofy User Div V4
+echo.
+echo.
+echo.
+echo.
+echo                   ----------------------------------------------------
+echo                     Bem vindo ao instalador do Lofy User Div V4         
+echo                     Voce esta prestes a instalar todos os requisitos  
+echo                     Em caso de duvida entre no grupo do discord 
+echo                   ----------------------------------------------------
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause nul
+cls
+color B
+cls
+call npm i
+cls
+color C
+echo.
+echo.
+echo.
+echo.
+echo                   ----------------------------------------------------
+echo                     Prontinho agora voce pode fechar o arquivo.
+echo                   ----------------------------------------------------
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause nul
+exit
